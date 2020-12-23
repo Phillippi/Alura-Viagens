77d94b8e6f49937f636d9027ffac37dbc77f2a45
Phillippi Aguiar <areias_2@hotmail.com>
2020-12-23 15:05:28 -0300

    Initial Commit

---
+++ Alura Viagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Phillippi Areias Aguiar on 12/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
